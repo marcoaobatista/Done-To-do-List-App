ec3132984ecb5240c6c463f24ffe6aa193160aab
Marco Antônio <marco.o.batista@gmail.com>
2023-06-20 18:25:26 -0300

    implemented BEM and finished overall elements of the application

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     let navOpenBtn = document.querySelector('.nav-open');
-    let navCloseBtn = document.querySelector('.nav-close');
+    let navCloseBtn = document.querySelector('.nav__close-btn');
 
     navOpenBtn.addEventListener('click', open_nav);
     navCloseBtn.addEventListener('click', close_nav);
 
     function open_nav(){
-        document.querySelector('nav').style.width = '250px';
-        document.querySelector('.main').style.marginLeft = '250px';
+        document.querySelector('.nav').style.width = '250px';
+        document.querySelector('.list').style.marginLeft = '250px';
     }
 
     function close_nav(){
-        document.querySelector('nav').style.width = '0';
-        document.querySelector('.main').style.marginLeft= '0';
+        document.querySelector('.nav').style.width = '0';
+        document.querySelector('.list').style.marginLeft= '0';
     }
 });
